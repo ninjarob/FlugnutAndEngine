d754e010304cf9dd6a723d1a5a84e4c59b8bdf84
cyril-thomas <cyril.satyam@gmail.com>
2013-06-25 10:57:10 -0600

    Clean up of the map scene, introducing the constants.

--- FlugnutAndEngine/src/com/pkp/flugnut/FlugnutAndEngine/screen/MapScene.java
+++ FlugnutAndEngine/src/com/pkp/flugnut/FlugnutAndEngine/screen/MapScene.java
@@ -2,6 +2,7 @@ package com.pkp.flugnut.FlugnutAndEngine.screen;
 
 import com.pkp.flugnut.FlugnutAndEngine.GLGame;
 import com.pkp.flugnut.FlugnutAndEngine.game.BaseGameScene;
+import com.pkp.flugnut.FlugnutAndEngine.utils.GameConstants;
 import org.andengine.entity.scene.background.SpriteBackground;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.input.touch.TouchEvent;
@@ -42,22 +43,22 @@ public class MapScene extends BaseGameScene {
     @Override
     public void initResources() {
         //FONT
-        FontFactory.setAssetBasePath("font/");
+        FontFactory.setAssetBasePath(GameConstants.ASSET_FONT_DIR);
         final ITexture fontTexture = new BitmapTextureAtlas(game.getTextureManager(), 256, 256, TextureOptions.BILINEAR);
-        this.mFont = FontFactory.createFromAsset(game.getFontManager(), fontTexture, game.getAssets(), "Droid.ttf", 16, true, android.graphics.Color.WHITE);
+        this.mFont = FontFactory.createFromAsset(game.getFontManager(), fontTexture, game.getAssets(), GameConstants.ASSET_FONT_DROID, 16, true, android.graphics.Color.WHITE);
         this.mFont.load();
 
-        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath(GameConstants.ASSET_GRAPHICS_DIR);
         //BACKGROUND
         this.backgroundTexture = new BitmapTextureAtlas(game.getTextureManager(), 800, 1200);
-        this.mapBackground = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.backgroundTexture, game, "map.png", 0, 0);
+        this.mapBackground = BitmapTextureAtlasTextureRegionFactory.createFromAsset(this.backgroundTexture, game, GameConstants.ASSET_MAP_FILE, 0, 0);
         this.backgroundTexture.load();
 
         //BACK BUTTON
-        //this.mBitmapTextureAtlas = new BitmapTextureAtlas(game.getTextureManager(), 129, 193, TextureOptions.BILINEAR);
         this.mBitmapTextureAtlas = new BitmapTextureAtlas(game.getTextureManager(), 129, 226, TextureOptions.BILINEAR);
-        this.buttonTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBitmapTextureAtlas, game, "buttons.png", 0, 0);
-        this.spriteTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBitmapTextureAtlas, game, "LevelSelect.png", 0, 193);
+        this.buttonTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBitmapTextureAtlas, game, GameConstants.ASSET_SETTING_BUTTONS, 0, 0);
+        this.spriteTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(mBitmapTextureAtlas, game, GameConstants.ASSET_LEVEL_BUTTONS, 0, 193);
+
         this.backButtonTextureRegion = TextureRegionFactory.extractFromTexture(mBitmapTextureAtlas, 64, 64, 64, 64);
         this.mBitmapTextureAtlas.load();
     }
@@ -72,7 +73,7 @@ public class MapScene extends BaseGameScene {
         initLevels();
 
         //BACK BUTTON
-        final Sprite backButton = new Sprite(10, GLGame.CAMERA_HEIGHT-74, backButtonTextureRegion, game.getVertexBufferObjectManager()) {
+        final Sprite backButton = new Sprite(10, GLGame.CAMERA_HEIGHT - 74, backButtonTextureRegion, game.getVertexBufferObjectManager()) {
             @Override
             public boolean onAreaTouched(final TouchEvent pSceneTouchEvent, final float pTouchAreaLocalX, final float pTouchAreaLocalY) {
                 game.setNewScene(new MainMenuScene(game));
---
+++ FlugnutAndEngine/src/com/pkp/flugnut/FlugnutAndEngine/utils/GameConstants.java
@@ -0,0 +1,19 @@
+package com.pkp.flugnut.FlugnutAndEngine.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cyrilthomas
+ * Date: 6/21/13
+ * Time: 11:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface GameConstants {
+    public static final String ASSET_FONT_DIR = "font/";
+    public static final String ASSET_FONT_DROID = "Droid.ttf";
+
+    public static final String ASSET_GRAPHICS_DIR = "gfx/";
+    public static final String ASSET_MAP_FILE = "map.png";
+    public static final String ASSET_SETTING_BUTTONS = "buttons.png";
+    public static final String ASSET_LEVEL_BUTTONS = "LevelSelect.png";
+
+}
