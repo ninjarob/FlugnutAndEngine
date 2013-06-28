4388d9f2bef8cc682a78f4f8e0a8761c5edb1860
Austin Haws <austinhaws@gmail.com>
2013-06-27 20:56:14 -0600

    removed #frames from scroller; instead it now determines how many pixels to move per second with an optimization factor to get the job done

--- FlugnutAndEngine/src/com/pkp/flugnut/FlugnutAndEngine/screen/AustinScene.java
+++ FlugnutAndEngine/src/com/pkp/flugnut/FlugnutAndEngine/screen/AustinScene.java
@@ -18,18 +18,20 @@ import org.andengine.opengl.texture.region.TextureRegionFactory;
 
 public class AustinScene extends BaseGameScene {
 
+	private static final float optimizationFactor = 5.0f; // instead of 1 pixel per move, move this factor of pixels (moving just one pixel per time was too processor intensive
 	private BitmapTextureAtlas imageBitmapTextureAtlas;
 	private Sprite imageSprite;
 	private RectF imageRect; // the rect of the full image
 	private RectF spriteSliderRect; // the current rectangle partial view of the entire image
 	private PointF imageStep;
 	private int moveModifier = 1; // -1 moves to the left
-	private int currentFrame = 0;
+	private PointF margin;// this is how many pixels it takes to move from left/top to right/bottom of the image in the view port
+	private float secondsPerPixel; // how many seconds to wait to move another pixel
 	// parameters
 	private final Point imageSize = new Point(736, 391);
 	private final Rect spriteRect = new Rect(10, 10, 200, 401); // rect to show of the image at a time; this gives both the position on the view and the height/width to show
-	private final int numberFrames = 20;
 	private final String imageFileName = "flugnut_title.png"; // 736X391
+	private final float secondsLength = 5.3f; // how long it takes to scroll from left to right of image
 
 	public AustinScene(GLGame game) {
 		super(game);
@@ -47,12 +49,14 @@ public class AustinScene extends BaseGameScene {
 		assert (imageRect.width() == imageSize.x && imageRect.height() == imageSize.y); // sanity check
 
 		// see how much space is left besides the view port and divide that in to steps
-		imageStep = new PointF((imageSize.x - spriteRect.width()) / numberFrames, (imageRect.height() - spriteRect.height()) / numberFrames);
+		margin = new PointF(imageSize.x - spriteRect.width(), imageRect.height() - spriteRect.height());
+		secondsPerPixel = (secondsLength / margin.x) * optimizationFactor;
+		imageStep = new PointF(1.0f * optimizationFactor, (margin.y / secondsLength) * secondsPerPixel * optimizationFactor); // always move 1 pixel in the x direction; do a ratio move in the y direction so that it concludes at the same time as the x direction
 
 		// set the current sprite to null because there is nothing being shown
 		imageSprite = null;
 
-		// set the current sprite view
+		// set the current sprite view region
 		spriteSliderRect = new RectF(0, 0, imageStep.x + spriteRect.width(), imageStep.y + spriteRect.height()); // image is always at top left
 
 		// show the current sprite
@@ -61,7 +65,7 @@ public class AustinScene extends BaseGameScene {
 
 	@Override
 	public void initScene() {
-		registerUpdateHandler(new TimerHandler(1.0f, true, new ITimerCallback() {
+		registerUpdateHandler(new TimerHandler(secondsPerPixel, true, new ITimerCallback() {
 			@Override
 			public void onTimePassed(final TimerHandler pTimerHandler) {
 				moveSprite();
@@ -71,20 +75,14 @@ public class AustinScene extends BaseGameScene {
 	}
 
 	private void moveSprite() {
-		currentFrame += moveModifier; // increment / decrement frame
-		if (currentFrame >= numberFrames) { // switch modifier so it bounces back and forth
-			currentFrame = numberFrames - 1;
-			moveModifier *= -1;
-		} else if (currentFrame < 0) {
-			currentFrame = 0;
-			moveModifier *= -1;
-		}
-
 		spriteSliderRect.left += imageStep.x * moveModifier;
 		spriteSliderRect.right += imageStep.x * moveModifier;
 		spriteSliderRect.top += imageStep.y * moveModifier;
 		spriteSliderRect.bottom += imageStep.y * moveModifier;
 
+		if (spriteSliderRect.left >= imageSize.x - spriteRect.width() || spriteSliderRect.left <= 0) {
+			moveModifier *= -1; // next move will go in the opposite direction
+		}
 	}
 
 	private void showSprite() {
@@ -93,13 +91,19 @@ public class AustinScene extends BaseGameScene {
 		// create the new sprite to show
 		ITextureRegion clippedRegion = TextureRegionFactory.extractFromTexture(imageBitmapTextureAtlas, (int) spriteSliderRect.left, (int) spriteSliderRect.top, (int) spriteSliderRect.width(), (int) spriteSliderRect.height());
 		tempSprite = new Sprite(spriteRect.left, spriteRect.top, clippedRegion, game.getVertexBufferObjectManager());
+		// show the new sprite
 		attachChild(tempSprite);
 
+		// remove the previous sprite
 		if (imageSprite != null) {
 			detachChild(imageSprite);
 		}
+
+		// remember the new sprite
 		imageSprite = tempSprite;
 	}
 	// combine clippedRegion replaces spritesliderrect
+	// instead of moving by lage chunks, move by one pixel but make  timer longer/shorter to be able to scroll the whole image tin time
 //	HE IS ALREADY SCROLLING ON THE MAIN PAGE!!!
+	// Later: set destination points and times between so that animation can jump around for timed intervals
 }
